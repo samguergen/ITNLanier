3734a98de13a44aabfe724b14fa869dd4fca6156
Samantha Guergenenov <samguergen@gmail.com>
2018-01-04 17:32:31 -0500

    operate section

---
+++ app.js
@@ -0,0 +1,14 @@
+// $(document).ready(()=>{
+//   console.log('jquery init');
+//   // $('.main').onload(()=>{
+//   //   console.log('main section loading');
+//   // 
+//   // })
+// 
+//   $('a').click(function(e) {
+//     e.preventDefault(); // stops the default action of clicking on the link
+//     var pageToLoad = $(this).attr('href'); // gets the href of the clicked link
+//     $('.main').load(pageToLoad); // loads the remote page into the content div without refresh
+//   });
+// 
+// })
